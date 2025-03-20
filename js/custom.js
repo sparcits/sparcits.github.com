26189f2a3e551b7dd28512c0ec8b9e986ba4297d
sparcITS <vaibhavsinghldp@gmail.com>
2025-03-21 00:01:16 +0530

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -33,5 +33,29 @@ $(function () {
 			$(this).toggleClass('active');
 		});
 	});
+
+	/*===============================================================
+		Horizontal Scroll
+	===============================================================*/
+	//Selecting Elements
+	let sticky = document.querySelector('.journey-item');
+	let stickyParent = document.querySelector('.journey-container');
+	let scrollWidth = sticky.scrollWidth;
+	let verticalScrollHeight = stickyParent.getBoundingClientRect().height - sticky.getBoundingClientRect().height;
+
+	$(document).on('scroll', function () {
+		let stickyPosition = sticky.getBoundingClientRect().top;
+		/* console.log(stickyPosition) */
+		if (stickyPosition > 130) {
+			return;
+		} else {
+			let scrolled = stickyParent.getBoundingClientRect().top; //how much is scrolled?
+			sticky.scrollLeft = (scrollWidth / verticalScrollHeight) * (-scrolled) * 0.85;
+		}
+	});
+
 });
+
+
+
 
